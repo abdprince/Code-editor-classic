d22ba82d5b4ce511e17ec60229538bf1b184c83a
Abdullrahman almaghrabi <73081830+abdprince@users.noreply.github.com>
2026-01-16 11:17:45 +0300

    Fix CSS background color for paragraph elements

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const editor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
 editor.setValue(`<!DOCTYPE html>
 <html>
 <style>
-p{background: #633;}
+p{background: #099;}
 </style>
   <body>
     <h1>مرحبا بك</h1>
